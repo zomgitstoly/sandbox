ead19d60412f6dd8fae7eb4af782e8c4d96281af
Anatoly Torchinsky <link13@gmail.com>
2012-03-13 22:22:05 -0700

    Project Euler Problem 2 Done

--- pe.py
+++ pe.py
@@ -7,28 +7,20 @@ def prob1():
         elif(i%5 == 0):
             sum += i
     print sum
-#Global Used
-class glb:
-    current = 0
-    count = 0
-    prev = 0
+
 #Problem 2
-def fib():
-    fib0 = 0
-    fib1 = 1
-    if (glb.count == 0): 
-        glb.count = glb.count +1
-        glb.current = fib0
-        return fib0
-    if (glb.count == 1):
-        glb.count = glb.count+1
-        glb.prev = fib0
-        glb.current = fib1
-        return fib1
-    if (glb.count == 2):
-        next = glb.current + glb.prev
-        glb.prev = glb.current
-        glb.current = next
-        return glb.current
-           
-    
+def fib2(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    sumf = 0
+    while b < n:
+        if((b%2) == 0):
+            sumf += b 
+        a, b = b, a+b
+    return sumf
+def main():
+    answer = fib2(4000000)
+    print answer
+
+if __name__ == '__main__':
+    main()
+
